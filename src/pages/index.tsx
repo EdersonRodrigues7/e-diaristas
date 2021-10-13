83c6257107ce2e88840b321add620a2ca0c468a1
EdersonRodrigues7 <ederson@trilhante.com.br>
2021-10-12 22:03:56 -0300

    hooks, services and API consuming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,29 @@
-import type { NextPage } from 'next';
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from '@components/data-display/UserInformation/UserInformation';
 import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
-import { Button, Typography, Container } from '@mui/material';
+import { Button, Typography, Container, CircularProgress } from '@mui/material';
 import {
   FormElementsContainer,
   ProfessionalsPaper,
   ProfessionalsContainer
 } from 'ui/styles/pages/index.style';
+import useIndex from 'data/hooks/pages/useIndex.page';
+import { ApiService } from 'data/services/ApiService';
 
 export default function Home() {
+  const {
+    cep,
+    setCep,
+    cepValido,
+    searchProfessionals,
+    error,
+    employees,
+    search,
+    loading,
+    remainingEmployees
+  } = useIndex();
+
   return (
     <div>
       <SafeEnvironment />
@@ -22,56 +35,57 @@ export default function Home() {
         <FormElementsContainer>
           <TextFieldMask
             mask={'99.999-999'}
-            value={'a'}
+            value={cep}
             label={'Digite seu CEP'}
             variant={'outlined'}
+            onChange={event => setCep(event.target.value)}
             fullWidth
           />
-          <Typography color={'error'}>CEP inválido</Typography>
-          <Button variant={'contained'} color={'secondary'} sx={{ width: '13.75rem' }}>
-            Buscar
+          {error && <Typography color={'error'}>{error}</Typography>}
+          <Button
+            variant={'contained'}
+            color={'secondary'}
+            sx={{ width: '13.75rem' }}
+            disabled={!cepValido || loading}
+            onClick={() => searchProfessionals(cep)}
+          >
+            {loading ? <CircularProgress size={20} /> : 'Buscar'}
           </Button>
         </FormElementsContainer>
-        <ProfessionalsPaper>
-          <ProfessionalsContainer>
-            <UserInformation
-              name={'Ederson'}
-              picture={'https://github.com/EdersonRodrigues7.png'}
-              rating={3}
-              description={'Campinas-SP'}
-            />
-            <UserInformation
-              name={'Ederson'}
-              picture={'https://github.com/EdersonRodrigues7.png'}
-              rating={3}
-              description={'Campinas-SP'}
-            />
-            <UserInformation
-              name={'Ederson'}
-              picture={'https://github.com/EdersonRodrigues7.png'}
-              rating={3}
-              description={'Campinas-SP'}
-            />
-            <UserInformation
-              name={'Ederson'}
-              picture={'https://github.com/EdersonRodrigues7.png'}
-              rating={3}
-              description={'Campinas-SP'}
-            />
-            <UserInformation
-              name={'Ederson'}
-              picture={'https://github.com/EdersonRodrigues7.png'}
-              rating={3}
-              description={'Campinas-SP'}
-            />
-            <UserInformation
-              name={'Ederson'}
-              picture={'https://github.com/EdersonRodrigues7.png'}
-              rating={3}
-              description={'Campinas-SP'}
-            />
-          </ProfessionalsContainer>
-        </ProfessionalsPaper>
+        {search &&
+          (employees.length > 0 ? (
+            <ProfessionalsPaper>
+              <ProfessionalsContainer>
+                {employees.map((item, index) => {
+                  return (
+                    <UserInformation
+                      key={index}
+                      name={item.nome_completo}
+                      picture={item.foto_usuario}
+                      rating={item.reputacao}
+                      description={item.cidade}
+                    />
+                  );
+                })}
+              </ProfessionalsContainer>
+              <Container sx={{ textAlign: 'center' }}>
+                {remainingEmployees > 0 && (
+                  <Typography sx={{ mt: 5 }}>
+                    ...e mais {remainingEmployees}{' '}
+                    {remainingEmployees > 1 ? 'profissionais' : 'profissional'} atendem ao seu
+                    endereço.
+                  </Typography>
+                )}
+                <Button variant={'contained'} color={'secondary'} sx={{ mt: 5 }}>
+                  Contratar um profissional
+                </Button>
+              </Container>
+            </ProfessionalsPaper>
+          ) : (
+            <Typography align={'center'} color={'textPrimary'}>
+              Ainda não temos nenhum funcionário disponível em sua região 😥
+            </Typography>
+          ))}
       </Container>
     </div>
   );
